0cc01bd75b62c3a46ddfc45386807960dfadc4ec
Oliver Munkebo <oliver.munkebo@ibm.com>
2022-11-19 11:37:11 +0100

    feat(svelte): added basic svelte setup

---
+++ client/src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let macros = [];
+
+  fetch('http://localhost:8000/macros').then((response) => response.json()).then(res => {
+    res.map(macro => {
+      macro.description = macro.description.split(/\r?\n/);
+    })
+
+    macros = res;
+  });
+
+</script>
+
+<main>
+  <h1>FF14 macro</h1>
+  <ul>
+    {#each macros as macro}
+      <li>
+        <p>{macro.name}</p>
+        {#each macro.description as line}
+          <pre class="ffxiv-text">{line}</pre>
+        {/each}
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+</style>
